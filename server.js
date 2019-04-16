01555b685b1db3d66bad52c476265adb875b9573
onewoorks <irwanbibrahim@onewoorks.com>
2019-04-16 22:39:55 +0000

    add sample download and sse

--- server.js
+++ server.js
@@ -9,9 +9,38 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (request, response) => {
-    response.send('Hello World')
+    //response.send('Hello World')
+    response.send("<a href='output/Excel_2.xlsx'>Report Generate</a>")
 })
+
+app.get('/download', function(req, res){
+  var file = __dirname + '/output/Excel_2.xlsx';
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/countdown', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  })
+  countdown(res, 1)
+})
+
+function countdown(res, count) {
+  res.write("data: " + count + "\n\n")
+  if (count)
+    setTimeout(() => countdown(res, count+1 ), 1000)
+  else
+    res.end()
+}
 
 
 app.post('/generate-report', (request, response) => {
